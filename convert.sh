73e8a482ea83c47b60f8f097410552e4c743f11f
CvvT <weitengchencc@gmail.com>
2020-01-10 19:48:58 -0800

    update

--- convert.sh
+++ convert.sh
@@ -1,4 +1,4 @@
 for d in cqe-challenges/*; do
     name=$(basename $d)
-	cp  ${d}/build/release/bin/${name} ../cgc2elf/cqe-challenges/
+	cp  ${d}/build/release/bin/${name} cgc2elf/cqe-challenges/
 done
